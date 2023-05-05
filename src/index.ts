5e7a475852d24274407110c9abc9c41009bbdbc4
khanhngguyen <ntkimkhanh97@gmail.com>
2023-05-05 16:22:43 +0300

    changed from fetch to axios, removed unused func

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
+import axios, { AxiosError } from 'axios';
+
 import Business from "./interfaces/Business";
 import Company from "./interfaces/Company";
 
 //generic function
 const getData = async <T> (endpoint: string) => {
   try {
-    const data = await fetch(endpoint);
-    const response = await data.json();
-    const result: T[] = response.results;
-    return result;
-  } catch (e) {
-    console.log(e);
+    const response = await axios.get(endpoint);
+    //console.log(response);
+    return response.data.results;
+  } catch (error: any) {
+    if (axios.isAxiosError(error) && error.response) {
+      console.log(error.response.status);
+      console.log(error.response.statusText);
+      console.log(error.response.data.results);
+      console.log('from response');
+    } else if (error.request) {
+      console.log('Bad request')
+    } else {
+      console.log('Error', error.message);
+    }
   }
 }
 
+
 //GET /BIS/V1
-const getCompany = async () => {
-  const data = await getData <Company> ("http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=2&resultsFrom=0&companyRegistrationFrom=2014-02-28");
-  //console.log(data);
-  return data;
-}
+// const getCompany = async () => {
+//   const data = await getData <Company> ("http://avoindata.prh.fi/bis/v1?totalResults=false&maxResults=2&resultsFrom=0&companyRegistrationFrom=2014-02-28");
+//   //console.log(data);
+//   return data;
+// }
 
 //getCompany();
 
@@ -26,8 +37,8 @@ const getCompany = async () => {
 const getBusiness = async (id: string) => {
   const data = await getData <Business> (`https://avoindata.prh.fi/bis/v1/${id}`);
   const util = require('util');
-  console.log(util.inspect(data, false, null, true));
-  //console.log(data);
+  //console.log(util.inspect(data, false, null, true));
+  console.log(data);
   //console.log(JSON.stringify(data, null, 4));
   return data;
 }
